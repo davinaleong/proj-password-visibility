8e8623579a5ff2ac223d978b2f76402513bdd77c
Davina Leong <davina.leong.sy@gmail.com>
2022-10-07 11:44:58 +0800

    Done with demo

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 console.log(`main.js loaded.`)
 
-const formEl = document.querySelector(`[data-element=form]`)
+const formExampleEl = document.querySelector(`[data-element=form-example]`)
 const passwordVisibilityEls = document.querySelectorAll(
   `[data-element=password-visibility]`
 )
+const formValuesEl = document.querySelector(`[data-element=form-values]`)
 
 passwordVisibilityEls.forEach((passwordVisibilityEl) => {
   passwordVisibilityEl.addEventListener(`click`, (e) => {
     console.log(e.target.tagName)
-    const formGroup = e.target.tagName === "BUTTON" ? e.target.parentElement : e.target.parentElement.parentElement;
+    const formGroup =
+      e.target.tagName === "BUTTON"
+        ? e.target.parentElement
+        : e.target.parentElement.parentElement
     console.log(formGroup.childNodes)
 
     if (formGroup.childNodes.length >= 3 && formGroup.childNodes[3]) {
@@ -24,4 +28,27 @@ passwordVisibilityEls.forEach((passwordVisibilityEl) => {
       passwordInputEl.setAttribute(`type`, inputType)
     }
   })
+})
+
+formExampleEl.addEventListener(`submit`, (e) => {
+  e.preventDefault()
+
+  const name = formExampleEl.elements["name"].value
+  const email = formExampleEl.elements["email"].value
+  const password = formExampleEl.elements["password"].value
+  const confirmPassword = formExampleEl.elements["confirm-password"].value
+
+  const formValuesHtml = `
+        <hr class="my-400">
+
+        <h2 class="fs-600">Form Result</h2>
+
+        <p>
+            Name: <strong>${name}</strong><br>
+            Email: <strong>${email}</strong><br>
+            Password: <strong>${password}</strong><br>
+            Confirm Password: <strong>${confirmPassword}</strong><br>
+        </p>
+    `
+  formValuesEl.innerHTML = formValuesHtml
 })
